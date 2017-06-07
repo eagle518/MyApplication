bcdd853962d6924f70b202d47f4db472f9abab36
liyiwei <365355497@qq.com>
2017-06-07 12:33:01 +0800

    gloalApplication

--- app/src/main/java/com/example/lyw/myapplication/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/lyw/myapplication/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.lyw.myapplication;
+package com.example.lyw.myapplication.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.lyw.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
